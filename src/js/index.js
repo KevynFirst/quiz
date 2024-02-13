dfa2122beaa05abcfdc90f0ed4abf3fe1551dcdc
Kevyn First <77193545+KevynFirst@users.noreply.github.com>
2024-02-13 13:20:05 -0300

    terceiro commit
    funções adicionais do projeto nlw

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //Estrutura de dados por uma Array
-const perguntasReact = [
+const perguntas = [
   {
     pergunta: "Qual é a finalidade do comando 'useState()' em React?",
     respostas: [
@@ -91,33 +91,38 @@ const perguntasReact = [
     correta: 1
   },
 ];
-  
+
   const quiz = document.querySelector('#quiz')
   const template = document.querySelector('template')
   //new Set() - permite armazenar coleções de valores únicos, onde cada valor pode aparecer apenas uma vez.
   const corretas = new Set()
-  const totalDePerguntas = perguntasReact.length
+  const totalDePerguntas = perguntas.length
   const acertos = document.querySelector('#acertos')
   const mostrarTotal = document.querySelector('#acertos span')
   //textContent - usada para obter ou definir o texto contido em um nó do DOM
   mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
 
+  const button = document.querySelector("button")
+  const modal = document.querySelector("dialog")
+  const buttonClose = document.querySelector(".close")
+  const resultModal = document.querySelector('dialog h2')
+
+
+
   
   
   // loop ou laço de repetição
-  for(const item of perguntasReact) {
-    
+  for(const item of perguntas) { 
     //cloneNode() - cria uma cópia exata de um nó existente sem modificar o original
     const quizItem = template.content.cloneNode(true)
     quizItem.querySelector('h3').textContent = item.pergunta
     
     for(let resposta of item.respostas) {
-      
       const dt = quizItem.querySelector('dl dt').cloneNode(true)
       dt.querySelector('span').textContent = resposta
       
       //setAttribute() - é usado para modificar atributos existentes ou adicionar novos atributos a elementos HTML
-      dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntasReact.indexOf(item) )
+      dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item) )
       dt.querySelector('input').value = item.respostas.indexOf(resposta)
 
       dt.querySelector('input').onchange = (event) => {
@@ -127,9 +132,13 @@ const perguntasReact = [
         if(estaCorreta){
           corretas.add(item)
         }
-        // if(corretas.size > 7){
-        //   acertos.style.backgroundColor = '#A3E635';
-        // }
+        
+       if(corretas.size >= 7){
+        resultModal.textContent = "Você passou!";
+       }else{
+        resultModal.textContent = "Você não passou!";
+       }
+
         mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
       }
 
@@ -142,6 +151,17 @@ const perguntasReact = [
     quiz.appendChild(quizItem)
   }
 
+
+  button.onclick = () => {
+    modal.showModal()
+  }
+
+  buttonClose.onclick = () => {
+    modal.close()
+    location.reload()
+  }
+
+  //resultModal.textContent = "haha"
   
 
   
